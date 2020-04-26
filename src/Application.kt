6dec1edc01a3f6e2d9030cbe1b9a2e7464528dd6
alanvianaa <alanviana.nascimento@gmail.com>
2020-04-26 16:59:42 -0300

    first commit

---
+++ src/Application.kt
@@ -0,0 +1,31 @@
+package com.alanviana
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.gson.*
+import io.ktor.features.*
+import java.util.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(ContentNegotiation) {
+        gson {
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText(UUID.randomUUID().toString(), contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/gson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
